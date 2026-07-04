771c9de44d232aa04b6579734ba8a97b443bb919
Fengyuan (Harry) Liu <fengyuan6666@gmail.com>
2026-07-04 16:54:16 -0700

    Contact page done and html layout for about

--- semisite/src/pages/About.jsx
+++ semisite/src/pages/About.jsx
@@ -2,9 +2,47 @@
 function About(){
     return (
         <div>
+            <h1>Hello! My name is Harry. Nice to meet you</h1>
             <div className="about">
-                <h1>About Harry</h1>
-                <h2></h2>
+                <div className="maincontent">
+                    <div className="overview">
+                        <h1>Achievements you would probably want to see from my journey thus far:</h1>
+                        <ul>
+                            <li>Spent 100 hours making a game with my friends to qualify for a hackathon in Shanghai</li>
+                            <li>Helped Organize Daydream Vancouver, a hackathon with 120 people.</li>
+                            <li>Half a year later, organized Campfire Vancouver as VP of Fraser Hack Club with 80 people attending</li>
+                            <li>Trained a Random Forest AI pipeline to find Cattle Camps in South Sudan using free environmental data at a 10m^2 / pixel scale for a challenge from the UN(in iteration)</li>
+                            <li>First time winning at a hackathon by making an agentic studying tool where you teach the AI.</li>
+                        </ul>
+                    </div>
+                    <div className="msg1">
+                        <h1>I have been building several exciting projects for a few months now! Some more ambitious, some less. Some with better progress, some more turbulent.</h1>
+                        <h1>Either way, the purpose of this site is to better document and share my work, my life, and perhaps a glimpse of who I am. </h1>
+                    </div>
+
+                    <div className="interests">
+                        <h1>Some things I like:</h1>
+                        <ul>
+                            <li>Flight simulators & aviation</li>
+                            <li>Building hardware</li>
+                            <li>Building software (I guess just building stuff I like in general)</li>
+                            <li>Exploring new places, near or far</li>
+                            <li>Going on mini-trips with my friends</li>
+                            <li>Hiking and walking</li>
+                            <li>Public transportation</li>
+                            <li>Strategy games</li>
+                            <li>Movies (some)</li>
+                            <li>Guns and armoured vehicles</li>
+                            <li>20th century Western Pop (ABBA, Michael Jackson, Falco, etc)</li>
+                            <li>Experimental pop (Tally Hall)</li>
+                            <li>Electronic music (Kraftwerk & YMO)</li>
+                            <li>Music in general, then</li>
+                            <li>Studying at library or with company</li>
+                            <li>... and so much more</li>
+                            
+                        </ul>
+                    </div>
+                </div>
             </div>
             
         </div>
--- semisite/src/pages/Contact.jsx
+++ semisite/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import '../css/contact.css'
-
-
+import githubimg from '../assets/github.png'
+import linkedinimg from '../assets/linkedin.webp'
+import semilogo from '../assets/SEMI_logo.svg'
 function Contact(){
     return(
         <div className="contact">
@@ -8,7 +9,23 @@ function Contact(){
             <div className="maincontent">
 
                 <div className="infobox">
-                    <h1>TEST</h1>
+                    <a href="https://github.com/fengyuan66" className="contactinfo">
+                        <img src={githubimg} />
+                        <h1>Github</h1>
+                    </a>
+                    <a href="https://www.linkedin.com/in/harry-liu-b768a0382/" className="contactinfo">
+                        <img src={linkedinimg} />
+                        <h1>Linkedin</h1>
+                    </a>
+                    <a href="mailto:fengyuan6666@gmail.com" className="contactinfo">
+                        <img src={semilogo} />
+                        <div className="emailbundle">
+                            <h1>Email</h1>
+                            <p>fengyuan6666@gmail.com</p>
+                        </div>
+                        
+                    </a>
+                    
                 </div>
 
                 <div className="formbox">
@@ -23,7 +40,7 @@ function Contact(){
                         <label htmlFor="message">Message</label>
                         <textarea id="message" name="message" rows="6" required />
 
-                        <button type="submit">Send</button>
+                        <button type="submit">✈</button>
                     </form>
                 </div>
 
