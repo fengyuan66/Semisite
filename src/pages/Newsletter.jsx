b8ac8f2ddd901a92af8bd610474bcd9a990ec1a1
Fengyuan (Harry) Liu <fengyuan6666@gmail.com>
2026-07-05 04:24:25 -0700

    Add demo newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,8 +1,23 @@
+import '../css/newsletter.css'
+
 function Newsletter(){
     return(
         <div className="newsletter">
             <h1>My Newsletter</h1>
             <p>There are currently no newsletters!</p>
+            <div className="newslettersbox">
+                <ul className="newsletterslist">
+                    <li>
+                        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className="newsletterlink">
+                        <div className="newsletterlinkbox">
+                            2069-06-07 | My Summer on Mars
+                        </div>
+                        </a>
+                        
+                    </li>
+                </ul>
+                
+            </div>
         </div>
     )
 }
