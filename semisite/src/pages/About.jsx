3d53708d0ef62800101c3e91a3840e2fe6c03228
Fengyuan (Harry) Liu <fengyuan6666@gmail.com>
2026-07-04 18:04:29 -0700

    Done with the website?!! Feat: Add the About Harry Page

--- semisite/src/pages/About.jsx
+++ semisite/src/pages/About.jsx
@@ -1,27 +1,32 @@
-
+import '../css/about.css'
+import harryimg from '../assets/harry.png'
 function About(){
     return (
         <div>
-            <h1>Hello! My name is Harry. Nice to meet you</h1>
+            <div className="abttop">
+                <img src={harryimg} />
+                <h1>Hello! My name is Harry. Nice to meet you!</h1>
+            </div>
+            
             <div className="about">
-                <div className="maincontent">
+                <div className="about_maincontent">
                     <div className="overview">
-                        <h1>Achievements you would probably want to see from my journey thus far:</h1>
+                        <h2>Achievements you would probably want to see from my journey thus far:</h2>
                         <ul>
-                            <li>Spent 100 hours making a game with my friends to qualify for a hackathon in Shanghai</li>
-                            <li>Helped Organize Daydream Vancouver, a hackathon with 120 people.</li>
-                            <li>Half a year later, organized Campfire Vancouver as VP of Fraser Hack Club with 80 people attending</li>
+                            <li>Spent 100 hours making a <a href="https://fraserhackclub.itch.io/spacegoose">game</a> with my friends to qualify for a hackathon in Shanghai</li>
+                            <li>Helped Organize <a href="https://daydream.bcydc.ca/?_gl=1*15thrw8*_ga*MTY2MjU5MjYyLjE3ODMxOTU4MDg.*_ga_JD2FNNVXTR*czE3ODMyMTI4OTYkbzIkZzAkdDE3ODMyMTI4OTgkajU4JGwwJGgw">Daydream Vancouver</a>, a hackathon with 120 people.</li>
+                            <li>Half a year later, organized <a href="https://campfire.hackclub.com/vancouver">Campfire Vancouver</a> as VP of Fraser Hack Club with 80 people attending</li>
                             <li>Trained a Random Forest AI pipeline to find Cattle Camps in South Sudan using free environmental data at a 10m^2 / pixel scale for a challenge from the UN(in iteration)</li>
                             <li>First time winning at a hackathon by making an agentic studying tool where you teach the AI.</li>
                         </ul>
                     </div>
                     <div className="msg1">
-                        <h1>I have been building several exciting projects for a few months now! Some more ambitious, some less. Some with better progress, some more turbulent.</h1>
-                        <h1>Either way, the purpose of this site is to better document and share my work, my life, and perhaps a glimpse of who I am. </h1>
+                        <p>I have been building several exciting projects for a few months now! Some more ambitious, some less. Some with better progress, some more turbulent. <br></br><br></br></p>
+                        <p>Either way, the purpose of this site is to better document and share my work, my life, and perhaps a glimpse of who I am. </p>
                     </div>
 
                     <div className="interests">
-                        <h1>Some things I like:</h1>
+                        <h2>Some things I like:</h2>
                         <ul>
                             <li>Flight simulators & aviation</li>
                             <li>Building hardware</li>
@@ -42,6 +47,11 @@ function About(){
                             
                         </ul>
                     </div>
+
+                    <div className="msg1">
+                        <p>SEMI was started as a <a href="https://trending.knowyourmeme.com/editorials/guides/what-does-larp-or-larping-mean-the-role-playing-terms-turned-slang-explained">LARP</a> when I was in grade 5 and jumped on the train of making projects with my friends. <br></br><b>Today, it is a symbol representing not myself, but my work, activity, and presence in the tech fields.</b></p>
+                    </div>
+
                 </div>
             </div>
             
--- semisite/src/pages/Newsletter.jsx
+++ semisite/src/pages/Newsletter.jsx
@@ -2,6 +2,7 @@ function Newsletter(){
     return(
         <div className="newsletter">
             <h1>My Newsletter</h1>
+            <p>There are currently no newsletters!</p>
         </div>
     )
 }
